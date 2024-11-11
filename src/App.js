48433d839a538362160b0d6a30f354cc68080e88
PabloReyes <andresreyes.ceo@gmail.com>
2024-11-10 20:59:41 -0500

    creacion proyecto pelis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="layout">
+        {/* <!--Cabecera--> */}
+        <header className="header">
+            <div classNameName="logo">
+                <div className="play"></div>
+            </div>
+
+            <h1>MisPelis</h1>
+        </header>
+
+        {/* <!--Barra de navegación--> */}
+        <nav className="nav">
+            <ul>
+                <li><a href="/#">Inicio</a></li>
+                <li><a href="/#">Peliculas</a></li>
+                <li><a href="/#">Blog</a></li>
+                <li><a href="/#">Contacto</a></li>
+            </ul>
+        </nav>
+
+        {/* <!--Contenido principal--> */}
+        <section id="content" className="content">
+
+            {/* <!--aqui van las peliculas--> */}
+            <article className="peli-item">
+                <h3 className="title">Desarrollo web</h3>
+                <p className="description">victorroblesweb.es</p>
+
+                <button className="edit">Editar</button>
+                <button className="delete">Borrar</button>
+            </article>
+
+            <article className="peli-item">
+                <h3 className="title">Desarrollo web</h3>
+                <p className="description">victorroblesweb.es</p>
+
+                <button className="edit">Editar</button>
+                <button className="delete">Borrar</button>
+            </article>
+
+            <article className="peli-item">
+                <h3 className="title">Desarrollo web</h3>
+                <p className="description">victorroblesweb.es</p>
+
+                <button className="edit">Editar</button>
+                <button className="delete">Borrar</button>
+            </article>
+
+            <article className="peli-item">
+                <h3 className="title">Desarrollo web</h3>
+                <p className="description">victorroblesweb.es</p>
+
+                <button className="edit">Editar</button>
+                <button className="delete">Borrar</button>
+            </article>
+
+        </section>
+
+        {/* <!--Barra lateral--> */}
+        <aside className="lateral">
+            <div className="search">
+                <h3 className="title">Buscador</h3>
+                <form>
+                    <input type="text" id="search_field" />
+                    <button id="search">Buscar</button>
+                </form>
+            </div>
+
+            <div className="add">
+                <h3 className="title">Añadir pelicula</h3>
+                <form>
+                    <input type="text" id="title" placeholder="Titulo" />
+                    <textarea id="description" placeholder="Descripción"></textarea>
+                    <input type="submit" id="save" value="Guardar" />
+                </form>
+            </div>
+        </aside>
+
+        {/* <!--Pie de página--> */}
+        <footer className="footer">
+            &copy; Máster en React - <a href="https://victorroblesweb.es">victorroblesweb.es</a>
+        </footer>
+
     </div>
+
   );
 }
 
